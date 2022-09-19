f48ea4dbc905e9cc6425ca1c94de9a7191f5e4f6
LeviXD <92174516+LEVIXDDLMAO@users.noreply.github.com>
2022-09-18 21:45:41 -0600

    and done

--- source/CreditsState.hx
+++ source/CreditsState.hx
@@ -109,7 +109,15 @@ class CreditsState extends MusicBeatState
 			['ninjamuffin99',		'ninjamuffin99',	"Programmer of Friday Night Funkin'",						'https://twitter.com/ninja_muffin99',	'F73838'],
 			['PhantomArcade',		'phantomarcade',	"Animator of Friday Night Funkin'",							'https://twitter.com/PhantomArcade3K',	'FFBB1B'],
 			['evilsk8r',			'evilsk8r',			"Artist of Friday Night Funkin'",							'https://twitter.com/evilsk8r',			'53E52C'],
-			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",							'https://twitter.com/kawaisprite',		'6475F3']
+			['kawaisprite',			'kawaisprite',		"Composer of Friday Night Funkin'",							'https://twitter.com/kawaisprite',		'6475F3'],
+			[''],
+			["Special thanks"],
+			['Sonic.EXE Team',   'sonicEXEteam'         "The best mod Creator Im sorry you had to cancel this ( Fly high divine )", 'https://www.reddit.com/r/FridayNightFunkin/' '0000FF'],
+			[''],
+			["Sonic.EXE Psych SC 2.5/3.0"],
+			['LeviXDD, 'levixd', "just an html port and coder",    'https://github.com/LEVIXDDLMAO'  'C30085'],
+			['MemeHoovy',           'memehoovy',        "A smart person<3",                                      'https://twitter.com/meme_hoovy',      'C30085']
+			
 		];
 		
 		for(i in pisspoop) {
